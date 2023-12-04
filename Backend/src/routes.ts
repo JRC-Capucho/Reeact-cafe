74e32303283d7129b8d097ca5cf6659372e59833
JRC-Capucho <joaorobertoc.capucho@gmail.com>
2023-12-03 21:58:17 -0300

    Routes for category and products

--- Backend/src/routes.ts
+++ Backend/src/routes.ts
@@ -1,15 +1,27 @@
-import { Router, Response, Request } from "express";
+import { Router } from "express";
+import { UserController } from "./controllers/user/UserController";
+import { AuthController } from "./controllers/user/auth/AuthController";
+import { isAuthenticated } from "./middlewares/isAuthenticated";
+import { CategoryController } from "./controllers/category/CategoryController";
+import { ProductController } from "./controllers/product/ProductController";
 
 const router = Router();
 
-router.get("/teste", (req: Request, res: Response) => {
-  throw new Error("Error");
-});
+// ROUTES FOR USER
+router.post("/user/create", new UserController().create); // Create
+router.post("/user/login", new AuthController().signin); // Authenticated
+router.get("/user/me", isAuthenticated, new UserController().detailUser); // information user
 
-router.get("/users");
-router.get("/user/:id");
-router.post("/user/create");
-router.put("/user/update/:id");
-router.delete("/user/delete/:id");
+// ROUTES FOR CATEGORY
+router.post(
+  "/category/create",
+  isAuthenticated,
+  new CategoryController().create,
+);
+
+router.get("/category", isAuthenticated, new CategoryController().list);
+
+// ROUTES FOR PRODUCTS
+router.post("/product/create", isAuthenticated, new ProductController().create);
 
 export { router };
